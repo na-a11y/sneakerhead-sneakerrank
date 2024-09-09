f2db8c76cfcfe2e6fe9e3e13f65fb6cff3a8392d
navmanak <iit2019066@iiita.ac.in>
2024-09-09 19:11:05 +0530

    sneakpeak

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -4,21 +4,17 @@ const { signup, login, checkLogin } = require('../controllers/authController');
 const authenticate = require('../middleware/authMiddleware');
 const { submitContactForm } = require('../controllers/contactController');
 
-// Route to handle contact form submission
-
+// Public Routes
 router.post('/signup', signup);
 router.post('/login', login);
 
-router.get('/check-login', (req, res) => {
-    // Logic to check if the user is logged in
-    const isLoggedIn = true; // Replace with actual logic
-    if (isLoggedIn) {
-      res.status(200).json({ message: 'User is logged in' });
-    } else {
-      res.status(401).json({ message: 'User is not logged in' });
-    }
-  });
-router.post('/contact', submitContactForm);
+// Protected Routes
+router.get('/check-login', authenticate, (req, res) => {
+    // Logic to check if the user is logged in based on token
+    res.status(200).json({ message: 'User is logged in', loggedIn: true });
+});
+
+// Protect the contact form submission route
+router.post('/contact', authenticate, submitContactForm);
 
 module.exports = router;
-
