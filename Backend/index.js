f21c19f257301ad93df0bca2f5370989782fa7a9
navmanak <iit2019066@iiita.ac.in>
2024-09-09 22:39:49 +0530

    sneakpeak

--- Backend/index.js
+++ Backend/index.js
@@ -21,6 +21,7 @@ app.use(express.json());
 app.use(cors({
   origin: ['http://localhost:3000', 'https://sneakerhead-sneakerrank-dy24.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 
