7297773a6b513127fb90efe2cc5d6cd4c4ac92dc
navmanak <iit2019066@iiita.ac.in>
2024-09-10 10:00:30 +0530

    sneakpeak

--- sneakerhead/src/api.js
+++ sneakerhead/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://sneakerhead-sneakerrank-xlcs.vercel.app/', 
+  baseURL: 'https://sneakerhead-sneakerrank-xlcs.vercel.app', 
   // Adjust base URL if needed
 });
 
